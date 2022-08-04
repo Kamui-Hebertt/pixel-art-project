610bcc98d970d3379db688de61ae5a1358a7602b
Hebertt Nascimento <heberttaferreira.com@gmail.com>
2022-08-04 19:38:41 -0300

    random colors

--- script.js
+++ script.js
@@ -3,9 +3,28 @@ function firstColorB() {
   firstPalette.classList.add('selected');
 };
 
-window.onload = firstColorB ();
+window.onload = firstColorB (); // chama a função depois que recarrega a página.
+
+
+function randomColor(){
+    const r = Math.random() * 255;
+    const g = Math.random() * 255;
+    const b = Math.random() * 255;
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+
 
 const colors = document.querySelectorAll('.color');
+
+function randomPalette (){
+    for(let i = 1; i < colors.length; i+=1){
+         colors[i].style.backgroundColor = randomColor();
+         
+    }
+}
+randomPalette();
 
 function removeClass() {
     for(let i = 0; i < colors.length; i+=1){
@@ -59,6 +78,14 @@ allPixel[i].style.backgroundColor = "white"
 } }
 
 clearBtn.addEventListener('click', clearPixels);
+
+let liChildren = document.querySelectorAll('li');
+console.log(liChildren);
+
+
+
+
+
 
 
 
